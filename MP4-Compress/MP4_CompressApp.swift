b885dfb36c5de74ea62d95ae4bd90d55590e068d
spencercap <spencercap@users.noreply.github.com>
2026-01-29 07:17:08 +0000

    Initial Commit

---
+++ MP4-Compress/MP4_CompressApp.swift
@@ -0,0 +1,17 @@
+//
+//  MP4_CompressApp.swift
+//  MP4-Compress
+//
+//  Created by spencer cap on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct MP4_CompressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
